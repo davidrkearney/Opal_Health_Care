e56204a835d170893f3b67738d2d302d078ee4fe
David Kearney <david.raymond.kearney@gmail.com>
2019-12-08 08:33:39 -0600

    update

---
+++ health_app/health_app/__init__.py
@@ -0,0 +1,25 @@
+"""
+health_app - Our Opal Application
+"""
+from opal.core import application
+from opal.core import menus
+
+class Application(application.OpalApplication):
+    javascripts   = [
+        'js/health_app/routes.js',
+    ]
+
+    menuitems = [
+        menus.MenuItem(
+            href="/#/list/", activepattern="/list/",
+            icon="fa-table", display="Lists",
+            index=0
+        ),
+        menus.MenuItem(
+            href='/pathway/#/add_patient',
+            display='Add Patient',
+            icon='fa fa-plus',
+            activepattern='/pathway/#/add_patient'
+        )
+    ]
+
